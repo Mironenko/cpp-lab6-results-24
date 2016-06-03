38b1d599eaabbe7f25a08bf72c9845da436e72f2
tha Mos Cape <da.moscape@gmail.com>
2016-06-03 23:00:45 +0300

    добавлен  container_cast

--- sources/al-6.h
+++ sources/al-6.h
@@ -6,3 +6,10 @@
 
 
 using namespace std;
+
+
+template <typename To, typename From>
+To container_cast(From && from) {
+    
+    return To(begin(from), end(from));
+}
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+#include <list>
 #include "QMatrix.h"
 
 
@@ -8,23 +10,29 @@ using namespace std;
 int main()
 	try {
 
-		ifstream ist("in.txt");
-		if (!ist) throw FileNotFound();
-		ofstream ost("out.txt");
-		if (!ost) throw FileNotFound();
+//		ifstream ist("in.txt");
+//		if (!ist) throw FileNotFound();
+//		ofstream ost("out.txt");
+//		if (!ost) throw FileNotFound();
+//
+//		auto a = QMatrix<Q,2,2>();
+//		ist >> a;
+//
+//		auto b = QMatrix<Q, 2, 2>();
+//		ist >> b;
+//
+//		QMatrix<Q, 2, 2> c = a + b;
+//		ost << c;
+//
+//		ist.close();
+//		ost.close();
 
-		auto a = QMatrix<Q,2,2>();
-		ist >> a;
-
-		auto b = QMatrix<Q, 2, 2>();
-		ist >> b;
-
-		QMatrix<Q, 2, 2> c = a + b;
-		ost << c;
-
-		ist.close();
-		ost.close();
-
+        vector<int> a;
+        a.push_back(3);
+        a.push_back(4);
+        
+        auto b = container_cast<list<double>>(a);
+        
 		return 0;
 		
 	}
